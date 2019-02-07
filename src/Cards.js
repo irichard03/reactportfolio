d832b77db3a4eb316f5e9fb6e2e8c19b2933d16f
Ian Richard <ianrichard03@gmail.com>
2019-02-07 02:25:12 -0600

    replaced ALL images with urls.

--- src/Cards.js
+++ src/Cards.js
@@ -3,63 +3,63 @@ const cards = [
         name: "DesolateHeadlands",
         link: "https://desolate-headland-11218.herokuapp.com/",
         repo: "https://github.com/irichard03/Project2",
-        image: "DesolateHeadlands.jpg"
+        image: "https://res.cloudinary.com/hicvm2new/image/upload/v1549527493/MyFolder/DesolateHeadlands.jpg"
     },
 
     {
         name: "Hooligans",
         link: "https://irichard03.github.io/Project1/",
         repo: "https://github.com/irichard03/Project1",
-        image: "fightGame.jpg"
+        image: "https://res.cloudinary.com/hicvm2new/image/upload/v1549527493/MyFolder/fightGame.jpg"
     },
 
     {
         name: "Giphy Star Trek",
         link: "https://irichard03.github.io/GiphyPuff/",
         repo: "https://github.com/irichard03/GiphyPuff",
-        image: "GiphyApi.jpg"
+        image: "https://res.cloudinary.com/hicvm2new/image/upload/v1549527493/MyFolder/GiphyAPi.jpg"
     },
 
     {
         name: "Train Scheduler",
         link: "https://irichard03.github.io/TrainScheduler/",
         repo: "https://github.com/irichard03/TrainScheduler",
-        image: "TrainScheduler.jpg"
+        image: "https://res.cloudinary.com/hicvm2new/image/upload/v1549527494/MyFolder/TrainScheduler.jpg"
     },
 
     {
         name: "Nerd Entanglement",
         link: "https://pure-headland-10733.herokuapp.com/",
         repo: "https://github.com/irichard03/friendFinder",
-        image: "Nerdentanglement.jpg"
+        image: "https://res.cloudinary.com/hicvm2new/image/upload/v1549527493/MyFolder/Nerdentanglement.jpg"
     },
 
     {
         name: "Burger NOM NOM",
         link: "https://radiant-atoll-26418.herokuapp.com/",
         repo: "https://github.com/irichard03/burger",
-        image: "Burger.jpg"
+        image: "https://res.cloudinary.com/hicvm2new/image/upload/v1549527493/MyFolder/Burger.jpg"
     },
 
     {
         name: "JQuery Ace",
         link: "https://irichard03.github.io/unit-4-game/",
         repo: "https://github.com/irichard03/unit-4-game",
-        image: "jqueryAce.jpg"
+        image: "https://res.cloudinary.com/hicvm2new/image/upload/v1549527494/MyFolder/jqueryAce.jpg"
     },
 
     {
         name: "Bamazon",
         link: "https://drive.google.com/file/d/1mXB3kjCu77fAxFVv8guxOyJTDTl9g3_C/view",
         repo: "https://github.com/irichard03/bamazon",
-        image: "nodeMysqlPOS.jpg"
+        image: "https://res.cloudinary.com/hicvm2new/image/upload/v1549527494/MyFolder/nodeMysqlPOS.jpg"
     },
 
     {
         name: "Word Guess Game",
         link: "https://irichard03.github.io/WordGuessGame/",
         repo: "https://github.com/irichard03/WordGuessGame",
-        image: "WordGuess.jpg"
+        image: "https://res.cloudinary.com/hicvm2new/image/upload/v1549527493/MyFolder/WordGuess.jpg"
     },
 
     {
