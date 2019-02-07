26779e810fb1cbda13f0741d746769cc213578fe
Ian Richard <ianrichard03@gmail.com>
2019-02-07 01:56:33 -0600

    living the dream.

--- src/content/Cards.js
+++ src/content/Cards.js
@@ -3,70 +3,70 @@ const cards = [
         name: "DesolateHeadlands",
         link: "https://desolate-headland-11218.herokuapp.com/",
         repo: "https://github.com/irichard03/Project2",
-        image: "/DesolateHeadlands.jpg"
+        image: "DesolateHeadlands.jpg"
     },
 
     {
         name: "Hooligans",
         link: "https://irichard03.github.io/Project1/",
         repo: "https://github.com/irichard03/Project1",
-        image: "/fightGame.jpg"
+        image: "fightGame.jpg"
     },
 
     {
         name: "Giphy Star Trek",
         link: "https://irichard03.github.io/GiphyPuff/",
         repo: "https://github.com/irichard03/GiphyPuff",
-        image: "/GiphyApi.jpg"
+        image: "GiphyApi.jpg"
     },
 
     {
         name: "Train Scheduler",
         link: "https://irichard03.github.io/TrainScheduler/",
         repo: "https://github.com/irichard03/TrainScheduler",
-        image: "/TrainScheduler.jpg"
+        image: "TrainScheduler.jpg"
     },
 
     {
         name: "Nerd Entanglement",
         link: "https://pure-headland-10733.herokuapp.com/",
         repo: "https://github.com/irichard03/friendFinder",
-        image: "/Nerdentanglement.jpg"
+        image: "Nerdentanglement.jpg"
     },
 
     {
         name: "Burger NOM NOM",
         link: "https://radiant-atoll-26418.herokuapp.com/",
         repo: "https://github.com/irichard03/burger",
-        image: "/Burger.jpg"
+        image: "Burger.jpg"
     },
 
     {
         name: "JQuery Ace",
         link: "https://irichard03.github.io/unit-4-game/",
         repo: "https://github.com/irichard03/unit-4-game",
-        image: "/jqueryAce.jpg"
+        image: "jqueryAce.jpg"
     },
 
     {
         name: "Bamazon",
         link: "https://drive.google.com/file/d/1mXB3kjCu77fAxFVv8guxOyJTDTl9g3_C/view",
         repo: "https://github.com/irichard03/bamazon",
-        image: "/nodeMysqlPOS.jpg"
+        image: "nodeMysqlPOS.jpg"
     },
 
     {
         name: "Word Guess Game",
         link: "https://irichard03.github.io/WordGuessGame/",
         repo: "https://github.com/irichard03/WordGuessGame",
-        image: "/WordGuess.jpg"
+        image: "WordGuess.jpg"
     },
 
     {
         name: "Vue Liche",
         link: "https://limitless-sea-54937.herokuapp.com/",
         repo: "https://github.com/irichard03/vueliche",
-        image: "/public/VueLiche.jpg"
+        image: "VueLiche.jpg"
     },
 ]
 
